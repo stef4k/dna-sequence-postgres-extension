8a2f579b177eabe6c0db540e9afeb3f362d3213d
stef4k <stef4k@gmail.com>
2024-11-03 15:07:51 +0100

    Add test queries

---
+++ test_queries.sql
@@ -0,0 +1,8 @@
+DROP EXTENSION IF EXISTS dna_sequence;
+CREATE EXTENSION dna_sequence;
+
+-- check if dna type works
+select dna_sequence('GATTACAaaaa');
+
+-- check type
+select pg_typeof(dna_sequence('GATTACAaaaa'));
