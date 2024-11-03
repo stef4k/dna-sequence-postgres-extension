d72ddf42842aff3cb34f36ab520ef6a9376aad84
stef4k <stef4k@gmail.com>
2024-11-03 18:51:17 +0100

    Add qkmer

--- dna_sequence_template/dna_sequence--1.0.sql
+++ dna_sequence_template/dna_sequence--1.0.sql
@@ -30,7 +30,29 @@ CREATE FUNCTION length(dna_sequence)
     RETURNS integer AS 'MODULE_PATHNAME', 'dna_sequence_length' 
     LANGUAGE C IMMUTABLE STRICT PARALLEL SAFE;
 
+-- Function to convert text to qkmer
+CREATE FUNCTION qkmer_in(cstring) RETURNS qkmer
+    AS 'MODULE_PATHNAME', 'qkmer_in'
+    LANGUAGE C IMMUTABLE STRICT PARALLEL SAFE;
 
+-- Function to convert qkmer to text
+CREATE FUNCTION qkmer_out(qkmer) RETURNS cstring
+    AS 'MODULE_PATHNAME', 'qkmer_out'
+    LANGUAGE C IMMUTABLE STRICT PARALLEL SAFE;
+
+-- Define the new qkmer type
+CREATE TYPE qkmer (
+    internallength = VARIABLE,
+    input = qkmer_in,
+    output = qkmer_out,
+    storage = extended, -- to compress and store large QKMER sequences efficiently
+    category = 'S' -- to classify it as a string-like type
+);
+
+-- Define the length function for qkmer
+CREATE FUNCTION length(qkmer)
+    RETURNS integer AS 'MODULE_PATHNAME', 'qkmer_length' 
+    LANGUAGE C IMMUTABLE STRICT PARALLEL SAFE;
 
 /******************************************************************************
  * Constructor
