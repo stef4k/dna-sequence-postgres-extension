c319018da6403ec66e60965131ab381e8daeebc7
owantland <ottog.wantland@gmail.com>
2024-11-17 16:09:47 +0100

    Updated db generator to accurately categorize the dna sequences as well as the output file for testing.

--- dna_db_generator/dna_db_generator.py
+++ dna_db_generator/dna_db_generator.py
@@ -3,30 +3,35 @@ import pandas as pd
 #File paths
 file_path = '.venv/files/SRR000001_2.fastq'
 
-# Read the generated .fastq file and return a list of dna sequences
-def query_parsing(file):
-    sequences = []
-    with open(file, 'r') as seq:
-        n = 0
-        val = False
-        for line in seq.read().splitlines():
-            if val:
-                sequences.append(line)
-                val = False
-                n+=1
-            # Validate the length of the dna sequence
-            else:
-                if line.startswith("@"):
-                    len = line.find("length=")
-                    len = line[len+7:]
-                    try:
-                        if int(len) <= 35:
-                            val = True
-                    except:
-                        val = False
+#Identifies the type of sequence as either DNA, Kmer or QKmer
+def seq_sorter(sequence, sequences):
+    if sequence.find("R") + sequence.find("N") + sequence.find("Y") >= 0:
+        sequences[sequence] = 'qkmer'
+    else:
+        if len(sequence) <= 32:
+            sequences[sequence] = 'kmer'
+        else:
+            sequences[sequence] = 'dna'
     return sequences
 
+
+# Read the generated .fastq file and return a list of dna sequences
+def query_parsing(file):
+    with open(file, 'r') as seq:
+        val = False
+        sequences = {}
+        n = 0
+        for line in seq.read().splitlines():
+            if val:
+                sequences = seq_sorter(line, sequences)
+                val = False
+            else:
+                if line.startswith("@") and line.find("length=") >= 0:
+                    val = True
+    return sequences
+
+
 seq = query_parsing('.venv/files/SRR000001_2.fastq')
-sequences =  pd.DataFrame(seq, columns=['SEQUENCE'])
+sequences =  pd.DataFrame(seq.items(), columns=['SEQUENCE', 'TYPE'])
 filename = f"sequences.csv"
 sequences.to_csv(filename, index=False)
