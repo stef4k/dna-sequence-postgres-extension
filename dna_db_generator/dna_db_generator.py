e4a4480d7bb43ca3d7e59c513395c1f307512282
owantland <ottog.wantland@gmail.com>
2024-12-04 13:07:42 +0100

    Simple script for parsing .fastq files from the NCBI database and generating a .csv file to test the dna extension. Will continue to update for further functionality.

--- dna_db_generator/dna_db_generator.py
+++ dna_db_generator/dna_db_generator.py
@@ -1,44 +1,78 @@
 import pandas as pd
+import os
+import psycopg2
+import subprocess
+
+subprocess.call(['./sra_toolkit/bin/fasterq-dump', '--split-files', 'SRR900001'])
 
 #File paths
-file_path = '.venv/files/SRR000001_2.fastq'
+sources_paths = []
+output_paths = []
+for file in os.listdir('files'):
+    if file.endswith(".fastq"):
+        sources_paths.append(os.path.join("files", file))
+
+#Databse connection
+def db_connect():
+    conn = psycopg2.connect(database="complex",
+                            host="127.0.0.1",
+                            user='owantland1',
+                            password='password',
+                            port='5432')
+    cursor = conn.cursor()
+    return cursor
 
 #Identifies the type of sequence as either DNA, Kmer or QKmer
-def seq_sorter(sequence, sequences):
+def seq_sorter(sequence, sequences, dnas, kmers, qkmers):
     if sequence.find("R") + sequence.find("N") + sequence.find("Y") >= -2:
         if len(sequence) <= 32:
             sequences[sequence] = 'qkmer'
+            qkmers.append(sequence)
         else:
             pass
     else:
         if len(sequence) <= 32:
             sequences[sequence] = 'kmer'
+            kmers.append(sequence)
         else:
             sequences[sequence] = 'dna'
-    return sequences
+            dnas.append(sequence)
+    return sequences, dnas, kmers, qkmers
 
 # Read the generated .fastq file and return a list of dna sequences
-def query_parsing(file):
-    with open(file, 'r') as seq:
-        val = False
-        sequences = {}
-        n = 0
-        for line in seq.read().splitlines():
-            if val:
-                sequences = seq_sorter(line, sequences)
-                val = False
-            else:
-                if line.startswith("@") and line.find("length=") >= 0:
-                    val = True
-    return sequences
+def query_parsing(files):
+    sequences = {}
+    dnas = []
+    kmers = []
+    qkmers = []
+    for file in files:
+        with open(file, 'r') as seq:
+            val = False
+            n = 0
+            for line in seq.read().splitlines():
+                if val:
+                    sequences, dnas, kmers, qkmers = seq_sorter(line, sequences, dnas, kmers, qkmers)
+                    val = False
+                else:
+                    if line.startswith("@") and line.find("length=") >= 0:
+                        val = True
+            seq.close()
+    return sequences, dnas, kmers, qkmers
 
 def command_generator(sequence):
-    with (open(".venv/files/dna_inserts.sql", 'w') as dna_file,
-          open('.venv/files/kmer_inserts.sql', 'w') as kmer_file,
-          open('.venv/files/qkmer_inserts.sql', 'w') as qkmer_file):
+    with (open("files/dna_inserts.sql", 'w') as dna_file,
+          open('files/kmer_inserts.sql', 'w') as kmer_file,
+          open('files/qkmer_inserts.sql', 'w') as qkmer_file):
         dna_file.truncate(0)
         kmer_file.truncate(0)
         qkmer_file.truncate(0)
+
+        # cursor = db_connect()
+        #
+        # #Drop and create the appropriate tables
+        # cursor.execute("DROP TABLE IF EXISTS DNAS;")
+        # cursor.execute("CREATE TABLE DNAS (DNA_SEQUENCE DNA_SEQUENCE);")
+
 
         dna_file.write("DROP TABLE IF EXISTS DNAS;\n")
         dna_file.write("CREATE TABLE DNAS (DNA_SEQUENCE DNA_SEQUENCE);\n")
@@ -77,9 +111,16 @@ def command_generator(sequence):
                     qkmer_file.write(f",('{seq}')\n")
 
 
-seq = query_parsing('.venv/files/SRR000001_2.fastq')
-command_generator(seq)
+seq, dna, kmer, qkmer = query_parsing(sources_paths)
+# command_generator(seq)
+pd_dna =  pd.DataFrame(dna, columns=['SEQUENCE'])
+filename = f"dna.csv"
+pd_dna.to_csv(filename, index=False)
 
-# sequences =  pd.DataFrame(seq.items(), columns=['SEQUENCE', 'TYPE'])
-# filename = f"sequences.csv"
-# sequences.to_csv(filename, index=False)
+pd_kmer =  pd.DataFrame(kmer, columns=['SEQUENCE'])
+filename = f"kmer.csv"
+pd_kmer.to_csv(filename, index=False)
+
+pd_qkmer =  pd.DataFrame(qkmer, columns=['SEQUENCE'])
+filename = f"qkmer.csv"
+pd_qkmer.to_csv(filename, index=False)
